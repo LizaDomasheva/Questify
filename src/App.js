fb1beac15f41f5317a61260da4d1402b6b71351d
Katrusy86 <katrin.myronova@gmail.com>
2020-06-03 15:20:18 +0300

    Added styles for all cards

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import Card from './components/card/Card'
-import CardChallenge from './components/card/CardChallenge'
+
 
 
 function App() {
   return (
-    <div >
-      <h1>App</h1>
-      <Card/>
-      <CardChallenge/>
-    </div>
+   <>
+   </>
   );
 }
 
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,41 +1,54 @@
-import React from 'react'
-import styled from './card.module.css'
-
+import React from "react";
+import styled from "./card.module.css";
 
 const Card = () => {
-    return (
-        <li className={styled.card_border}>
-            <div className={styled.card_header}>
-            <div className={styled.card_select}>Easy
+  return (
+    <div className={styled.card_list}>
+      <li className={styled.card_border}>
+        <div className={styled.card_header}>
+          <div className={styled.card_select}>
+            Easy
             <div className={styled.card_select_icon}></div>
-            </div>
-            <div className={styled.star_icon}></div>
-            </div>
+          </div>
+          <div className={styled.star_icon}></div>
+        </div>
+        <div className={styled.card_wrapper}>
             <div className={styled.card_container}>
-                <input className={styled.card_input} type="text" placeholder="Enter quest name" name="text" required/>
-                <div className={styled.date}>
-                    <input className={styled.card_input__date} type="text" value="02.06.2020 10:13 PM"/>
-                    <button className={styled.card_btn__icon}></button>
-                    {/* <div>Calendar</div> */}
-                </div>            
+            <input
+                className={styled.card_input}
+                type="text"
+                placeholder="Enter quest name"
+                name="text"
+                required
+            />
+            <div className={styled.date}>
+                <input
+                className={styled.card_input__date}
+                type="text"
+                value="02.06.2020 10:13 PM"
+                />
+                <button className={styled.card_btn__icon}></button>
+                {/* <div>Calendar</div> */}
+            </div>
             </div>
             <div className={styled.card_block}>
-                <div className={styled.card_category}>
-                    productivity</div>
-                <div className={styled.card_btn__create}>
-                    {/* <button className={styled.delete}></button> 
-                    <div className={styled.strip}></div>              
-                    <button className={styled.start}>Start</button> */}
+            <div className={styled.card_category}>productivity</div>
+            <div className={styled.card_btn__create}>
+                {/* <button className={styled.delete}></button> 
+                        <div className={styled.strip}></div>              
+                        <button className={styled.start}>Start</button> */}
 
                 <button className={styled.save}></button>
-                    <div className={styled.strip}></div>
+                <div className={styled.strip}></div>
                 <button className={styled.delete}></button>
-                    <div className={styled.strip}></div>
-                <button className={styled.done}></button>               
-                </div>
+                <div className={styled.strip}></div>
+                <button className={styled.done}></button>
             </div>
-        </li>
-    
-)}
+            </div>
+        </div>
+      </li>            
+    </div>
+  );
+};
 
 export default Card;
