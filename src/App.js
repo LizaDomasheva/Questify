b535521b1e7a360352d5f5e2fdfe68eac22e6bb3
Katrusy86 <katrin.myronova@gmail.com>
2020-06-03 03:03:29 +0300

    Added Card and CardChallenge components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Card from './components/card/Card'
+import CardChallenge from './components/card/CardChallenge'
 
 
 function App() {
   return (
     <div >
       <h1>App</h1>
+      <Card/>
+      <CardChallenge/>
     </div>
   );
 }
