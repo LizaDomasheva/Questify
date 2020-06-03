985370108e991a298f11fd5dd2ffa43f4fc8ea65
Yelyzaveta Domasheva <lizidomasheva@gmail.com>
2020-06-03 10:55:59 +0300

    some corrects

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import {LoginPage} from './pages/LoginPage';
+import {DashboardPage} from './pages/DashboardPage';
 
 
 function App() {
   return (
     <div >
-      <h1>App</h1>
+      <h1>Questify</h1>
+      {/* <Switch>
+      <Route exact to='/login' component={LoginPage}/>
+      <Route exact to='/dashboard' component={DashboardPage}/>
+      </Switch> */}
     </div>
   );
 }
