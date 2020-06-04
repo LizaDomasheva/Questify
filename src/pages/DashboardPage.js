a9dffe58b10febfd527df54bf01a6535e008394a
irynailina <irynailina@gmail.com>
2020-06-04 23:44:38 +0300

    delete/create

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -2,16 +2,23 @@ import React from "react";
 import { connect, useDispatch } from "react-redux";
 import Header from "../components/header/Header";
 import CardList from "../components/cardList/CardList";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getUser } from "../redux/operations";
 import { createCard } from "../redux/dashboardOperations";
 
 const DashboardPage = ({ nickname, todayCard }) => {
-  // console.log('todayCard', todayCard)
+  const [editFlag, seteditFlag] = useState(false);
+  console.log("typeof(editFlag)", typeof editFlag);
   const dispatch = useDispatch();
+
   const createNewCard = () => {
-    dispatch(createCard());
+    if (!editFlag) {
+      dispatch(createCard());
+      seteditFlag(true);
+    }
+    console.log("editFlag", editFlag);
   };
+
   useEffect(() => {
     dispatch(getUser());
   }, []);
