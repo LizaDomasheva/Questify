b390400cb67b87411519d0a78689c1fdf1224865
Yelyzaveta Domasheva <lizidomasheva@gmail.com>
2020-06-05 14:33:52 +0300

    button corrects

--- src/components/createQuestButton/createQuestButton.js
+++ src/components/createQuestButton/createQuestButton.js
@@ -2,20 +2,28 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './CreateQuestButton.module.css';
 
-function CreateQuestButton({ handleClick, isOpen }) {
-  console.log('handleClick :>> ', handleClick);
-    return (
-      <button 
-      // className={styles.createButton}
-        className={clsx(
-          styles.createButton,
-          !isOpen ? styles.active__button : styles.passive__button,
-        )}
-        onClick={handleClick}
-      >
-        +
-      </button>
-    );
-  }
-  
-  export default CreateQuestButton;
+const CreateQuestButton = ({ onClick = () => null }) => (
+  <button className={styles.Button} type="button" onClick={onClick}>
+    +
+  </button>
+);
+
+export default CreateQuestButton;
+
+// function CreateQuestButton({ handleClick, isOpen }) {
+//   console.log('handleClick :>> ', handleClick);
+//     return (
+//       <button
+//       // className={styles.createButton}
+//         className={clsx(
+//           styles.createButton,
+//           !isOpen ? styles.active__button : styles.passive__button,
+//         )}
+//         onClick={handleClick}
+//       >
+//         +
+//       </button>
+//     );
+//   }
+
+//   export default CreateQuestButton;
--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { connect, useDispatch } from "react-redux";
-import Header from "../components/header/Header";
-import CardList from "../components/cardList/CardList";
-import { useEffect, useState } from "react";
-import { getUser } from "../redux/operations";
-import { createCard } from "../redux/dashboardOperations";
+import React from 'react';
+import { connect, useDispatch } from 'react-redux';
+import Header from '../components/header/Header';
+import CardList from '../components/cardList/CardList';
+import { useEffect, useState } from 'react';
+import { getUser } from '../redux/operations';
+import { createCard } from '../redux/dashboardOperations';
 import CreateQuestButton from '../components/createQuestButton/CreateQuestButton';
 
 const DashboardPage = ({ nickname, todayCard }) => {
   const [editFlag, seteditFlag] = useState(false);
-  console.log("typeof(editFlag)", typeof editFlag);
+  console.log('typeof(editFlag)', typeof editFlag);
   const dispatch = useDispatch();
 
   const createNewCard = () => {
@@ -17,14 +17,12 @@ const DashboardPage = ({ nickname, todayCard }) => {
       dispatch(createCard());
       seteditFlag(true);
     }
-    console.log("editFlag", editFlag);
+    console.log('editFlag', editFlag);
   };
 
   useEffect(() => {
     dispatch(getUser());
   }, []);
-
-  
 
   return (
     <>
@@ -33,13 +31,13 @@ const DashboardPage = ({ nickname, todayCard }) => {
       {/* <CardChallenge />
       <CompletedChallenge/>
       <CompletedModal/>  */}
-      <CreateQuestButton onClick={() => createNewCard()}/>
+      <CreateQuestButton onClick={createNewCard} />
       {/* <button onClick={() => createNewCard()}>CREATE NEW CARD</button> */}
     </>
   );
 };
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   nickname: state.user.nickname,
   todayCard: state.dashboard.today,
 });
