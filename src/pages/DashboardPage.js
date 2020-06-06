0fbdace14fff4512355d98f69328a059ad414c06
Yelyzaveta Domasheva <lizidomasheva@gmail.com>
2020-06-06 09:58:16 +0300

    some corrects

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -8,7 +8,7 @@ import { createCard } from '../redux/dashboardOperations';
 import CreateQuestButton from '../components/createQuestButton/CreateQuestButton';
 import styled from './DashboardPage.module.css'
 
-const DashboardPage = ({ nickname, todayCard, allTheRest, tomorrow, done }) => {
+const DashboardPage = ({ nickname, todayCard, allTheRest }) => {
   const [editFlag, seteditFlag] = useState(false);
   console.log('typeof(editFlag)', typeof editFlag);
   const dispatch = useDispatch();
@@ -32,9 +32,7 @@ const DashboardPage = ({ nickname, todayCard, allTheRest, tomorrow, done }) => {
       <h3 className={styled.title}>TODAY</h3>
       {todayCard.length > 0 && <CardList arr={todayCard} />}
       <h3 className={styled.title}>TOMORROW</h3>
-      {/* {tomorrow.length > 0 ? <CardList arr={tomorrow} /> : 'oooooooops!'} */}
       <h3 className={styled.title}>DONE</h3>
-      {done.length > 0 ? <CardList arr={done} /> : 'ooooooooops!'}
       <h3 className={styled.title}>ALL THE REST</h3>
       {allTheRest.length > 0 && <CardList arr={allTheRest} />}
       </div>
@@ -52,8 +50,6 @@ const mapStateToProps = state => ({
   nickname: state.user.nickname,
   todayCard: state.dashboard.today,
   allTheRest: state.dashboard.allTheRest,
-  tomorrow: state.dashboard.tomorrow,
-  done: state.dashboard.done
 });
 
 export default connect(mapStateToProps)(DashboardPage);
