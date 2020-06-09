ac3915a55042e38d14b28c3361c07f17c3e67153
Katrusy86 <katrin.myronova@gmail.com>
2020-06-09 11:15:20 +0300

    Updated select

--- src/components/card/Select.js
+++ src/components/card/Select.js
@@ -1,18 +1,23 @@
-import React from 'react'
-import styled from './select.module.css'
+import React from "react";
+import styled from "./select.module.css";
 
-
-
-
-const Select = props => {
- 
-    return(
-<select className={styled[props.defaultSelectGroupClr]} onChange={props.onSelectColor}>
-  <option value="easy" className={styled.easy_select}>Easy</option>
-    <option value="normal" className={styled.normal_select}>Normal</option>
-    <option value="hard" className={styled.hard_select}>Hard</option>
-</select> 
-
-)}
+const Select = (props) => {
+  return (
+    <select
+      className={styled[props.defaultSelectGroupClr]}
+      onChange={props.onSelectColor}
+    >
+      <option value="easy" className={styled.easy_select}>
+        Easy
+      </option>
+      <option value="normal" className={styled.normal_select}>
+        Normal
+      </option>
+      <option value="hard" className={styled.hard_select}>
+        Hard
+      </option>
+    </select>
+  );
+};
 
 export default Select;
---
+++ src/components/card/colourOptions.js
@@ -0,0 +1,5 @@
+export const colourOptions = [
+    { value: 'easy', label: 'Easy', color: '#47aad5', isFixed: true },
+    { value: 'normal', label: 'Normal', color: '#24d40c',  },
+    { value: 'hard', label: 'Hard', color: '#db0837' },
+  ];
